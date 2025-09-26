21098507cc54cde958415f4b61537c6c341037af
taliakusmirek <kusmire@bc.edu>
2025-09-26 14:50:51 -0400

    fix

--- myportfolio/src/components/Atmosphere.tsx
+++ myportfolio/src/components/Atmosphere.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 // Minimal no-op Atmosphere. We are not using the 3D scene right now, so keep this
 // lightweight and free of @react-three/fiber/three dependencies to prevent build errors.
 export function Atmosphere() {
